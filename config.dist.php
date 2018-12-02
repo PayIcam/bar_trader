5f3de8ef1a93d2990094dc330599493f3fe56ea2
GregoireGiraud <gregoiregiraud1@gmail.com>
2018-12-02 18:40:27 +0100

    ça ne sert plus à rien

--- config.dist.php
+++ config.dist.php
@@ -1,13 +1,4 @@
 <?php
-global $bdd_login;
-global $bdd_password;
-global $bdd_url;
-global $bdd_database;
-
-$bdd_login = "";
-$bdd_password = "";
-$bdd_url = "";
-$bdd_database = "";
 
 $_CONFIG = [
     'payicam' => [
